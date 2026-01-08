d5ffff6de353e35da627e91868b9f1f4e4ae89b8
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2026-01-08 17:32:22 +0200

    feat: add --debug flag to enable logging to debug.log (disabled by default)

--- cmd/root.go
+++ cmd/root.go
@@ -5,19 +5,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	debugFlag bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "lazylogcat",
 	Short: "Interactive Android logcat viewer",
 	Long:  `lazylogcat is an interactive TUI application for viewing Android device logs.`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-		return app.SetupLogging()
+		if debugFlag {
+			return app.SetupLogging()
+		}
+		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return app.LaunchTUI()
 	},
 	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
-		return app.CloseLog()
+		if debugFlag {
+			return app.CloseLog()
+		}
+		return nil
 	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&debugFlag, "debug", false, "Enable debug logging to debug.log file")
 }
 
 func Execute() error {
