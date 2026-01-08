677905be3034c507bf40f105b6960d01762e3e44
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2026-01-08 17:40:52 +0200

    feat: add version command

---
+++ cmd/version.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	Version = ""
+)
+
+func getVersion() string {
+	// If version was set via ldflags
+	if Version != "" {
+		return Version
+	}
+
+	// Otherwise, try to get version from build info (when installed via go install)
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
+	}
+
+	return "dev"
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(getVersion())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
